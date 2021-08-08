da972276068a0a884ea166ed932183f4c5833cab
lentz25 <66398745+lentz25@users.noreply.github.com>
2021-08-07 18:51:34 -0700

    added social page and chat feature

--- scripts/sript.js
+++ scripts/sript.js
@@ -11,6 +11,19 @@ function siteLogoClick() {
     window.location = "https://photo.santosla.com";
 }
 
-function test() {
-    console.log(document.getElementsByClassName("prices"));
+$(window).resize(function() {
+    var container_width = $('#pageContainer').width();
+    $('#pageContainer').html('<div class="fb-page" data-href="https://www.facebook.com/facebook" data-width="' + container_width + '" data-height="250" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/facebook"><a href="https://www.facebook.com/facebook">Facebook</a></blockquote></div></div>');
+    FB.XFBML.parse();
+});
+
+function changeFBPagePlugin(width, height, url) {
+
+    if (!isNaN(width) && !isNaN(height)) {
+        $(".fb-page").attr("data-width", width).attr("data-height", height);
+    }
+    if (url) {
+        $(".fb-page").attr("data-href", url);
+    }
+    FB.XFBML.parse();
 }
