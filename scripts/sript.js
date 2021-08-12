b7341922f0561d533454315d9ce3cd93b7be7740
lentz25 <66398745+lentz25@users.noreply.github.com>
2021-08-11 18:10:52 -0700

    fixed slideshow sizing and script placement
    jquery goes on top
    js goes on bottom

--- scripts/sript.js
+++ scripts/sript.js
@@ -27,3 +27,20 @@ function changeFBPagePlugin(width, height, url) {
     }
     FB.XFBML.parse();
 }
+
+// Slideshow
+
+var slideIndex = 1;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    $('.mySlides').eq(slideIndex - 1).css('display', 'block')
+    setTimeout(carousel, 3000); // Change image every 3 seconds
+}
